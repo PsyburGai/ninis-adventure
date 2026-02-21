71519cbb9ceb6f175dd0d5f23075b47d85394b25
Robert <robertlharrisiv@gmail.com>
2026-02-21 18:54:55 -0500

    Fix tileset_grass_dirt.tsx - remove incorrect tileoffset y=-16

--- assets/tilesets/tileset_grass_dirt.tsx
+++ assets/tilesets/tileset_grass_dirt.tsx
@@ -1,6 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="tileset_grass_dirt" tilewidth="16" tileheight="16" tilecount="16" columns="4">
- <tileoffset x="0" y="-16"/>
  <image source="tileset_grass_dirt.png" width="64" height="64"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
