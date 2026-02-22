b002065d28f004dcd1266b9ed1ab9808c5371af1
Robert <robertlharrisiv@gmail.com>
2026-02-21 20:04:24 -0500

    Add portal.tsx so portal appears as tileset in Tiled

---
+++ assets/tilesets/objects/portal/portal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="portal" tilewidth="16" tileheight="48" tilecount="1" columns="1">
+ <image source="portal.png" width="16" height="48"/>
+</tileset>
