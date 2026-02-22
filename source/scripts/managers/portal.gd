47a9798ae8c5c113bbb9a6973ea32436bb2e84b3
Robert <robertlharrisiv@gmail.com>
2026-02-22 00:28:49 -0500

    Portal activation on UP arrow press, add move_up input action

--- source/scripts/managers/portal.gd
+++ source/scripts/managers/portal.gd
@@ -1,7 +1,7 @@
 class_name portal
 extends Area2D
 
-## portal - teleports Nini to the target scene.
+## portal - teleports Nini to the target scene when player presses UP.
 ## Class "portal" (lowercase) matches Tiled object class.
 ## Object name "portal_r" = right side exit, "portal_l" = left side exit.
 ## Required Tiled properties: target_scene
@@ -11,17 +11,32 @@ extends Area2D
 @export var target_spawn: String = ""
 @export var is_end_portal: bool = false
 
+var player_inside: bool = false
+
 func _ready():
 	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body: Node) -> void:
-	if not body.is_in_group("player"):
+	if body.is_in_group("player"):
+		player_inside = true
+
+func _on_body_exited(body: Node) -> void:
+	if body.is_in_group("player"):
+		player_inside = false
+
+func _process(_delta: float) -> void:
+	if not player_inside:
 		return
+	if Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("move_up"):
+		_enter_portal()
+
+func _enter_portal() -> void:
 	if target_scene == "":
 		push_warning(name + " has no target_scene set!")
 		return
 
-	# Infer spawn side from portal name if target_spawn not set in Tiled
+	# Infer spawn side from portal name if target_spawn not set
 	if target_spawn == "":
 		if name == "portal_r":
 			target_spawn = "left"
