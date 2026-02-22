080ee905aba0fdd3e19528a05f1b22b0bda2f37e
Robert <robertlharrisiv@gmail.com>
2026-02-22 00:35:59 -0500

    Add animated portal - 6 frame spritesheet, portal.tscn scene, wired into level_1_1

--- assets/tilesets/objects/portal/portal.tsx
+++ assets/tilesets/objects/portal/portal.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="portal" tilewidth="16" tileheight="48" tilecount="1" columns="1">
- <image source="portal.png" width="16" height="48"/>
+<tileset version="1.10" tiledversion="1.11.2" name="portal" tilewidth="16" tileheight="48" tilecount="6" columns="6">
+ <image source="portal.png" width="96" height="48"/>
 </tileset>
